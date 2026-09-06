0d7796e5873fe67975dccb320a90cd152e4b431c
GloboPatro <mrt0d@proton.me>
2026-09-06 17:54:09 +0000

    hybris: mount libselinux stubs for odm and product
    Change-Id: I0eb34132827d1593accea075bb1b9dccd8fdb651

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 touch /odm/lib64/libselinux_stubs.so; mount -o bind /usr/libexec/droid-hybris/system/lib64/libselinux_stubs.so /odm/lib64/libselinux_stubs.so
-touch /product/lib64/libselinux_stubs.so; mount -o bind /usr/libexec/droid-hybris/system/lib64/libselinux_stubs.so /product/lib64/libselinux_stubs.so
+mount -o bind /usr/libexec/droid-hybris/system/lib64/libselinux_stubs.so /product/lib64/libselinux_stubs.so
